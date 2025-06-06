f0252540b4033bad854e01fa85471e3b4e101590
angelomp2001 <apassala@gmail.com>
2025-06-06 08:53:18 -0500

    update/ save csv as df v1

--- python.py
+++ python.py
@@ -1,4 +1,9 @@
 # get data
-#download and save historical data from yahoo finance
+# download and save historical data from yahoo finance
+import pandas as pd
 import yfinance as yf
 
+df = pd.read_csv('Equities Universe - Symbols.csv',
+                 index_col=0, parse_dates=True)
+
+print(df.head())
