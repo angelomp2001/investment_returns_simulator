866c12122dfc66f3e111571d83baee5954b5db5a
angelomp2001 <apassala@gmail.com>
2025-06-08 09:15:35 -0500

    test vs code

--- python.py
+++ python.py
@@ -3,7 +3,13 @@
 import pandas as pd
 import yfinance as yf
 
+# trying to use bigframes
+# import bigframes.bigquery
+# import bigframes.pandas as bpd
+# bpd.options.display.progress_bar = None
+# df = bpd.read_gbq("gs://'investment returns simulator'/symbols_universe.csv")
+
+
 df = pd.read_csv('Equities Universe - Symbols.csv',
                  index_col=0, parse_dates=True)
-
 print(df.index)
